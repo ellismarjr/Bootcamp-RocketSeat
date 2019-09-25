a7841e84f04f7e6c56fadbd61d98608491c0691e
Júnior de Oliveira <ellismarjr@hotmail.com>
2019-09-25 15:12:41 -0300

    Commit

--- gobarber/backend/src/database/index.js
+++ gobarber/backend/src/database/index.js
@@ -1,21 +1,25 @@
-import Sequelize from 'sequelize'
+import Sequelize from 'sequelize';
 
-import User from '../app/models/User'
+import User from '../app/models/User';
+import File from '../app/models/File';
+import Appointment from '../app/models/Appointment';
 
-import databaseConfig from '../config/database'
+import databaseConfig from '../config/database';
 
-const models = [User];
+const models = [User, File, Appointment];
 
 class Database {
   constructor() {
-    this.init()
+    this.init();
   }
 
   init() {
-    this.connection = new Sequelize(databaseConfig)
+    this.connection = new Sequelize(databaseConfig);
 
-    models.map(model => model.init(this.connection))
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
-export default new Database()
+export default new Database();
---
+++ modulo05/frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Júnior de Oliveira</h1>
+    </div>
+  );
+}
+
+export default App;
